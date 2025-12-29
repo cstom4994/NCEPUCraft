1d3c1ff4fdf6a4316ed357f48827530731cd3061
RukiaOvO <1403555427@qq.com>
2025-12-29 17:07:15 +0800

    fix: TpaAll context.

--- src/main/java/net/kaoruxun/ncepucore/commands/TpaAllCommand.java
+++ src/main/java/net/kaoruxun/ncepucore/commands/TpaAllCommand.java
@@ -14,7 +14,7 @@ public final class TpaAllCommand extends BasicCommand {
     public boolean callback(CommandSender sender, String[] args) {
         if (!(sender instanceof Player)) return false;
         final Player sp = (Player) sender;
-        final String str = "  §d玩家 §f" + sp.getName() + " §d希望传送到你这里:";
+        final String str = "  §d玩家 §f" + sp.getName() + " §d希望你传送到他那里:";
         instance.getServer().getOnlinePlayers().forEach(it -> instance.requestTeleport(it, str,
                 () -> instance.delayTeleport(it, sp.getLocation())));
         sender.sendMessage("§a成功向玩家发送传送请求!");
